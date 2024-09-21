521069cf4fa0bc8c3bad5093162af533927158b5
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-09-21 18:59:42 +0600

    Добавил фикстуры

--- work-api/fixtures.ts
+++ work-api/fixtures.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import config from './config';
 import User from './models/User';
+import Product from './models/Product';
+import Category from './models/Category';
 
 const run = async () => {
   await mongoose.connect(config.database);
@@ -8,26 +10,65 @@ const run = async () => {
 
   try {
     await db.dropCollection('users');
-    // await db.dropCollection('');
+    await db.dropCollection('products');
   } catch (e) {
     console.log('Skipping drop...');
   }
 
   const firstUser = new User({
-    username: 'First user',
+    username: 'JOHn',
+    displayName: 'John',
+    phoneNumber: '213213123213',
     password: '123',
   });
   const secondUser = new User({
-    username: 'Second user',
-    password: '321',
+    username: 'Tommas',
+    displayName: 'Tommy',
+    phoneNumber: '54213213213',
+    password: '123',
   })
 
   firstUser.generateToken();
   secondUser.generateToken();
-  await firstUser.save();
-  await secondUser.save()
-
+  
+  const [category1, category2, category3] = await Category.create(
+    {
+      title: "Car",
+    }, {
+      title: "Computer",
+    }, {
+      title: "Other",
+    }
+  )
+  
+  await Product.create(
+    {
+      title: "Iphone11",
+      description: 'text',
+      price: '432',
+      image: 'fixtures/iphone11.jpeg',
+      category: category1,
+      idUser: firstUser,
+    }, {
+      title: "Mersedes-s-class",
+      description: 'text',
+      price: '1233',
+      image: 'fixtures/mers.jpeg',
+      category: category2,
+      idUser: firstUser,
+    }, {
+      title: "Iphone11",
+      description: 'text',
+      price: '432',
+      image: 'fixtures/car.jpeg',
+      category: category3,
+      idUser: secondUser,
+    }
+  )
+  
   await db.close();
+  await firstUser.save();
+  await secondUser.save();
 };
 
 run().catch(console.error);
